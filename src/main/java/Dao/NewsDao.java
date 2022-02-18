6ca1cf17dfead85e4cb508daba198c1041565797
WairimuThuku <lucythuku33@gmail.com>
2022-02-18 10:45:44 +0300

    update of the imports declaration of the interface

--- src/main/java/Dao/NewsDao.java
+++ src/main/java/Dao/NewsDao.java
@@ -1,4 +1,24 @@
 package Dao;
 
+import Models.DepartmentNews;
+import Models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    //create
+
+    void addNews(News news);
+    void addDepartmentNews(DepartmentNews departmentnews);
+
+    //read
+    List<News> getAll();
+
+    News findById(int id);
+
+    //update
+
+    //delete
+
+    void clearAll();
 }
