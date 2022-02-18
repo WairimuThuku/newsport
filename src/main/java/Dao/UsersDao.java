462f00caffc24bf55b96e7703e352d43b938705b
WairimuThuku <lucythuku33@gmail.com>
2022-02-18 10:43:11 +0300

    update of the imports declaration of the interface

--- src/main/java/Dao/UsersDao.java
+++ src/main/java/Dao/UsersDao.java
@@ -1,4 +1,25 @@
 package Dao;
 
+import Models.Departments;
+import Models.Users;
+
+import java.util.List;
+
 public interface UsersDao {
+
+    //create
+
+    void  add(Users user);
+
+    //read
+
+    List<Users> getAll();
+    List<Departments> getAllUserDepartments(int user_id);
+    Users findById(int id);
+
+    //update
+
+    //delete
+
+    void clearAll();
 }
